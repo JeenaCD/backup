567e852a1c69f52a26fb50edaf9eb895301678b1
JeenaCD <102554603+JeenaCD@users.noreply.github.com>
2022-04-19 12:13:47 +0530

    new

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 
 var us,em,pn,ps,cps;
+
+// username validation
 function username(){
     var a=document.getElementById('uservalue').value;
     if(a==""){
@@ -12,6 +14,7 @@ function username(){
     }
 }
 
+// email validation
 function email(){
     var c=document.getElementById('emailvalue').value;
     var d=/^([A-Za-z0-9\.-_]+)@([A-Za-z0-9\-]+)\.([A-Za-z]{2,3})((\.[A-Za-z]{2,3})?)$/;
@@ -25,6 +28,7 @@ function email(){
     }
 }
 
+// Phone number validation
 function phonenumber(){
     var e=document.getElementById('phonevalue').value;
     var f=/^([\w]{0}[\d]{10})$|^([\w]{0}[\d]{3}[\.][\d]{3}[\.][\d]{4})$|^([\w]{0}[\d]{3}[\-][\d]{3}[\-][\d]{4})$|^([\w]{0}[\d]{3}[\s][\d]{3}[\s][\d]{4})$/;
@@ -38,6 +42,7 @@ function phonenumber(){
     }
 }
 
+// Password validation
 function password(){
     var g=document.getElementById('passvalue').value;
     var h= /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
@@ -51,6 +56,7 @@ function password(){
     }
 }
 
+// Confirm password validation and checking whether password and confirm password same
 function cpassword(){
     var p=document.getElementById('passvalue').value;
     var i=document.getElementById('cpassvalue').value;
@@ -64,15 +70,18 @@ function cpassword(){
     cps=0;
     }
 }
+
+
+// validating signup form
 function signupvalidate(){
-    if(us==1 && em== 1){
+        if(us==1 && em== 1){
         if(pn==1 && ps==1){
             if(cps==1){
             return true;
             }
             else
             return false;
-        }
+            }
     else
     return false;
     }
@@ -80,6 +89,8 @@ function signupvalidate(){
     return false;
 }
 
+
+// validating login form
 function loginvalidate(){
     if(em==1 && ps==1){
         return true;
@@ -96,85 +107,100 @@ let weak = document.querySelector('#passwordStrength #weak');
 let strong = document.querySelector('#passwordStrength #strong');
 let passwordInfo = document.getElementById('passwordInfo');
 
+
+// Regular Expression for poor password, weak password and strong password
 let poorRegExp = /[a-z]/;
 let weakRegExp = /(?=.*?[0-9])/;;
 let strongRegExp = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
 let whitespaceRegExp = /^$|\s+/;
 
+// Checking password strength
 function strengthchecker(){
-
-     let passwordValue= passwordInput.value;
-     let passwordLength= passwordValue.length;
-
-     let poorPassword= passwordValue.match(poorRegExp);
-     let weakPassword= passwordValue.match(weakRegExp);
-     let strongPassword= passwordValue.match(strongRegExp);
-     let whitespace= passwordValue.match(whitespaceRegExp);
-
-if(passwordValue != ""){
-
- passwordStrength.style.display = "block";
- passwordStrength.style.display = "flex";
- passwordInfo.style.display = "block";
- passwordInfo.style.color = "black";
-
- if(whitespace)
- {
-  passwordInfo.textContent = "whitespaces are not allowed";
- }else{
- poorPasswordStrength(passwordLength, poorPassword, weakPassword, strongPassword);
- weakPasswordStrength(passwordLength, poorPassword, weakPassword, strongPassword);
- strongPasswordStrength(passwordLength, poorPassword, weakPassword, strongPassword);
+            let passwordValue= passwordInput.value;
+            let passwordLength= passwordValue.length;
+            let poorPassword= passwordValue.match(poorRegExp);
+            let weakPassword= passwordValue.match(weakRegExp);
+            let strongPassword= passwordValue.match(strongRegExp);
+            let whitespace= passwordValue.match(whitespaceRegExp);
+            
+            if(passwordValue != ""){
+                passwordStrength.style.display = "block";
+                passwordStrength.style.display = "flex";
+                passwordInfo.style.display = "block";
+                passwordInfo.style.color = "black";
+                if(whitespace){
+                    passwordInfo.textContent = "whitespaces are not allowed";
+                }else{
+                    poorPasswordStrength(passwordLength, poorPassword, weakPassword, strongPassword);
+                    weakPasswordStrength(passwordLength, poorPassword, weakPassword, strongPassword);
+                    strongPasswordStrength(passwordLength, poorPassword, weakPassword, strongPassword);
+                }
+            }else{
+                passwordStrength.style.display = "none";
+                passwordInfo.style.display = "none";
+            }
 }
-
- 
-}else{
- 
- passwordStrength.style.display = "none";
- passwordInfo.style.display = "none";
-
-}
-}
-
+        
+// checking for poor password
 function poorPasswordStrength(passwordLength, poorPassword, weakPassword, strongPassword){
+        if(passwordLength <= 3 && (poorPassword || weakPassword || strongPassword)){
+            poor.classList.add("active");
+            passwordInfo.style.display = "block";
+            passwordInfo.style.color = "red";
+            passwordInfo.textContent = "Your password is too Poor";
+        }
+}
 
-  if(passwordLength <= 3 && (poorPassword || weakPassword || strongPassword))
-    {
-   poor.classList.add("active");
-   passwordInfo.style.display = "block";
-   passwordInfo.style.color = "red";
-   passwordInfo.textContent = "Your password is too Poor";
-      
+// checking for weak password
+function weakPasswordStrength(passwordLength, poorPassword, weakPassword, strongPassword){
+        if(passwordLength>= 4 && poorPassword && (weakPassword || strongPassword))
+        {
+            poor.classList.remove("active");
+            weak.classList.add("active");
+            passwordInfo.textContent = "Your password is Weak";
+            passwordInfo.style.color = "orange";
+        }
+}
+
+// checking for strong password
+function strongPasswordStrength(passwordLength, poorPassword, weakPassword, strongPassword){
+        if(passwordLength >= 7 && (poorPassword && weakPassword) && strongPassword)
+        {
+            poor.classList.remove("active");
+            weak.classList.remove("active");
+            strong.classList.add("active");
+            passwordInfo.textContent = "Your password is strong";
+            passwordInfo.style.color = "green";
+        }
+}
+
+
+function Toggle1() {
+    var temp = document.getElementById("passvalue");
+    const eye = document.querySelector('#password-eye1');
+    if (temp.type === "password") {
+        temp.type = "text";
+        eye.classList.remove('fa-eye');  
+        eye.classList.add('fa-eye-slash');
+    }
+    else {
+        temp.type = "password";
+        eye.classList.add('fa-eye');  
+        eye.classList.remove('fa-eye-slash');
     }
 }
 
-function weakPasswordStrength(passwordLength, poorPassword, weakPassword, strongPassword){
-if(passwordLength>= 4 && poorPassword && (weakPassword || strongPassword))
-{
- weak.classList.add("active");
- passwordInfo.textContent = "Your password is Weak";
- passwordInfo.style.color = "orange";
-
-}else{
- weak.classList.remove("active");
- 
+function Toggle2() {
+    var temp = document.getElementById("cpassvalue");
+    const eye = document.querySelector('#password-eye2');
+    if (temp.type === "password") {
+        temp.type = "text";
+        eye.classList.remove('fa-eye');  
+        eye.classList.add('fa-eye-slash');
+    }
+    else {
+        temp.type = "password";
+        eye.classList.add('fa-eye');  
+        eye.classList.remove('fa-eye-slash');
+    }
 }
-}
-
-function strongPasswordStrength(passwordLength, poorPassword, weakPassword, strongPassword){
-
-if(passwordLength >= 7 && (poorPassword && weakPassword) && strongPassword)
-{
- poor.classList.add("active");
- weak.classList.add("active");
- strong.classList.add("active");
- passwordInfo.textContent = "Your password is strong";
- passwordInfo.style.color = "green";
-}else{
- strong.classList.remove("active");
- 
-}
-}
-
-
-
